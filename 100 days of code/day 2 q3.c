e5459bfbaab5ff22dae1d17cd1779c2e811641f7
piyush-14648 <piyushvij244@gmail.com>
2026-08-12 18:10:47 +0530

    Create day 2 q3.c

---
+++ 100 days of code/day 2 q3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int peri,area,l,b;
+    printf("Enter length and breadth: ");
+    scanf("%d %d", &l, &b);
+    peri=2*(l+b);
+    area=l*b;
+    printf("the perimeter of rectangle is %d\n",peri);
+    printf("the area of reectangle is %d\n",area);
+    return 0;
+}
