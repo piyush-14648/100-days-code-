910bf8df57a80e52775f2d3168338c984ae0e9d5
piyush-14648 <piyushvij244@gmail.com>
2026-08-13 22:25:49 +0530

    Create day 4 q8.c

---
+++ 100 days of code/day 4 q8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  int n,sum;
+  printf("enter value of n: ");
+  scanf("%d",&n);
+  sum=(n*(n+1))/2;
+  printf("Sum Of First n Natural Numbers is:%d",sum);
+  return 0;
+}
