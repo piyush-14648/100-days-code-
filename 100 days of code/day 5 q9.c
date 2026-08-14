ba38c50eb3050bd93f3d96f8752e69727ed408cd
piyush-14648 <piyushvij244@gmail.com>
2026-08-14 18:22:48 +0530

    Update day 5 q9.c

--- 100 days of code/day 5 q9.c
+++ 100 days of code/day 5 q9.c
@@ -6,12 +6,12 @@ int main() {
   printf("enter principal,rate and time: ");
   scanf("%d %d %d",&p,&r,&t);
   si=(p*r*t)/100;
-  ci=p*pow(1+r/100.00,t)-p;
+  ci=p*pow(1+(r/100.00),t)-p;
   printf("simple interest = %.2f\n",si);
   printf("compound interest = %.2f\n",ci);
     return 0;
 }
 // Method 2:
 // take r as float 
-// ci=p*pow(1+r/100,t)-p;
+// ci=p*pow(1+(r/100),t)-p;
 // It will give the same output
