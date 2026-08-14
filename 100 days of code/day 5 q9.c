7003ec475834dd6caabc61d5477038a6ee675138
piyush-14648 <piyushvij244@gmail.com>
2026-08-14 18:13:01 +0530

    Create day 5 q9.c

---
+++ 100 days of code/day 5 q9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+  int p,r,t;
+  float si,ci;
+  printf("enter principal,rate and time: ");
+  scanf("%d %d %d",&p,&r,&t);
+  si=(p*r*t)/100;
+  ci=p*pow(1+r/100.01,t)-p;
+  printf("simple interest = %.2f\n",si);
+  printf("compound interest = %.2f\n",ci);
+    return 0;
+}
