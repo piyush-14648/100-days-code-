7552c70e3aacca0a94fd0898680db013282d4185
piyush-14648 <piyushvij244@gmail.com>
2026-08-14 18:14:17 +0530

    Update day 5 q9.c

--- 100 days of code/day 5 q9.c
+++ 100 days of code/day 5 q9.c
@@ -6,7 +6,7 @@ int main() {
   printf("enter principal,rate and time: ");
   scanf("%d %d %d",&p,&r,&t);
   si=(p*r*t)/100;
-  ci=p*pow(1+r/100.01,t)-p;
+  ci=p*pow(1+r/100.00,t)-p;
   printf("simple interest = %.2f\n",si);
   printf("compound interest = %.2f\n",ci);
     return 0;
