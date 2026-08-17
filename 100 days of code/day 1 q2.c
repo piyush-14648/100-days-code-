781163f750e6966d68c49d96af0a180db731aa75
piyush-14648 <piyushvij244@gmail.com>
2026-08-17 13:03:33 +0530

    Update day 1 q2.c

--- 100 days of code/day 1 q2.c
+++ 100 days of code/day 1 q2.c
@@ -1,5 +1,5 @@
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
 #include <stdio.h>
-
 int main() {
     int a, b;
     printf("Enter two numbers: ");
