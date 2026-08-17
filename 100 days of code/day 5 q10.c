218dbb6d4230d803b3ed60cee03e99298dbd2a3e
piyush-14648 <piyushvij244@gmail.com>
2026-08-17 13:09:06 +0530

    Update day 5 q10.c

--- 100 days of code/day 5 q10.c
+++ 100 days of code/day 5 q10.c
@@ -1,5 +1,5 @@
+//Write a program to input time in seconds and convert it to hours:minutes:seconds format.
 #include <stdio.h>
-#include <math.h>
 int main() {
  int t_sec;
  printf("Enter time in seconds:");
