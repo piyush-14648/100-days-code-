ba73db80b982420c18d38ff7b836798d3b902e17
piyush-14648 <piyushvij244@gmail.com>
2026-08-30 10:58:13 +0530

    Update day 21 q41.c

--- 100 days of code/day 21 q41.c
+++ 100 days of code/day 21 q41.c
@@ -9,10 +9,12 @@ int main()
 
     digit=log10(n);     //it tells place value ka power,indirectly telling digits=4+1=5 , for n=12345 -->digit=4.09=>4
     divide=round(pow(10,digit));  //10000   ,used round function to truncate the value
+
     first=n/divide;       //12345/10000=1
     n=n%divide;          //12345%10000=2345
     end=n%10;           //2345%10=5
     n=n/10;            //2345/10=234
+    
     swap=end*divide + n*10 + first;    //5*10000 + 2340*10 + 1
     printf("swap value is %d",swap);
     return 0;
