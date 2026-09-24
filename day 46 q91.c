40e24f4a961b2a3ec6736c8e73a194848836f97e
piyush-14648 <piyushvij244@gmail.com>
2026-09-24 08:44:08 +0530

    Create day 46 q91.c

---
+++ day 46 q91.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for(i = 0; i < strlen(str); i++)
+    {
+        if(str[i] != 'a' && str[i] != 'e' &&
+           str[i] != 'i' && str[i] != 'o' &&
+           str[i] != 'u' && str[i] != 'A' &&
+           str[i] != 'E' && str[i] != 'I' &&
+           str[i] != 'O' && str[i] != 'U')
+        {
+            printf("%c", str[i]);
+        }
+    }
+
+    return 0;
+}
+
+//METHOD 2
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     char str[100];
+//     int i;
+
+//     printf("Enter a string: ");
+//     gets(str);
+
+//     for(i = 0; i < strlen(str); i++)
+//     {
+//         if(str[i] != 'a' && str[i] != 'e' &&
+//            str[i] != 'i' && str[i] != 'o' &&
+//            str[i] != 'u' && str[i] != 'A' &&
+//            str[i] != 'E' && str[i] != 'I' &&
+//            str[i] != 'O' && str[i] != 'U')
+//         {
+//             printf("%c", str[i]);
+//         }
+//     }
+
+//     return 0;
+// }
